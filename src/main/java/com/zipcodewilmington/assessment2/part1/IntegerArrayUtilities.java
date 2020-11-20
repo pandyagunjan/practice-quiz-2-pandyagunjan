14b72a0ef1fdb6ca4f23f73f2824b43aac63d743
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-20 15:54:55 -0500

    part 1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,18 +2,30 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return (array.length%2==0);
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        int length = (stop+1) - start;
+
+        Integer[] range = new Integer[length];
+        for (int i = 0; i < range.length; i++)
+        {
+            range[i] = start;
+            start++;
+        }
+        return range;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        Integer storeResult=array[0]+array[1];
+
+        return (array[0]+array[1]);
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        int length =array.length;
+        return (array[length-2]*array[length-1]);
     }
 }
