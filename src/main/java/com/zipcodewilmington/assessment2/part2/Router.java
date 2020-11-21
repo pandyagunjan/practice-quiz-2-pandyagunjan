3fc76a41f443f71b9cc50c191b53a03ccff73995
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-21 16:38:42 -0500

    test

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,26 +1,47 @@
 package com.zipcodewilmington.assessment2.part2;
 
-import java.util.HashMap;
+import com.sun.tools.corba.se.idl.StringGen;
+
+import java.util.*;
 import java.util.Map;
 
 public class Router {
-    Map<String,String> hp=new HashMap<String,String>();
+    HashMap<String,String> hp;
+
+    public Router() {
+        this.hp=new LinkedHashMap<>();
+    }
+
     public void add(String path, String controller) {
         this.hp.put(path,controller);
 
     }
 
     public Integer size() {
-        return null;
+        return hp.size();
     }
 
     public String getController(String path) {
-        return null;
+        return hp.get(path);
     }
 
     public void update(String path, String studentController) {
+        this.hp.replace(path ,studentController );
     }
 
     public void remove(String path) {
+        this.hp.remove(path);
     }
+    @Override
+    public String toString()
+    {
+        StringBuilder str= new StringBuilder();
+
+        for (Map.Entry<String,String> entry : hp.entrySet()) {  // Iterate through hashmap
+          str.append(entry.getKey()).append(entry.getValue()).append("\n");
+        }
+
+       return str.toString();
+    }
+
 }
